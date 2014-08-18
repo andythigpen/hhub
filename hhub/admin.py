dc4630c6361b077307266996943526c4821d61eb
andythigpen <andyt05@gmail.com>
2014-08-17 19:24:22 -0500

    Add plugin list to main page.

--- hhub/admin.py
+++ hhub/admin.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
 from flask_bootstrap import Bootstrap
 from hhub.config import get_default_cfg
+from hhub.registry import discover_plugins
 
 cfg = get_default_cfg()
 
@@ -8,8 +9,14 @@ app = Flask(__name__)
 app.debug = cfg.get('debug', False)
 Bootstrap(app)
 
+plugins = discover_plugins(cfg)
+for registry, cls in plugins:
+    if getattr(cls, 'admin', None):
+        app.register_blueprint(cls.admin, url_prefix='/%s' % cls.plugin_id)
+
 @app.route('/')
 def index():
+    g.plugins = discover_plugins(cfg)
     return render_template('index.html')
 
 if __name__ == '__main__':
